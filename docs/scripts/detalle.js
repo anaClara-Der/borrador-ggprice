5b1cc8a0dea188afa6bb067d1f41abf537174f53
AnaC <anaclaraderario@gmail.com>
2025-05-01 22:53:08 -0300

    Se puede ir a la página de detalle. Falta la comparación de precios entre sitios

---
+++ docs/scripts/detalle.js
@@ -0,0 +1,49 @@
+const API_KEY = "4b1742eb29634e329d7fd29447d706ca";
+// Obtener el ID de la URL
+const obtenerIdDeURL = () => {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("id");
+};
+
+//Conectar con la api rawg para pedir mas detalles
+const pedirDetalles = (juegoId) => {
+  fetch(`https://api.rawg.io/api/games/${juegoId}?key=${API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      pintarDetalles(data);
+    });
+};
+
+const pintarDetalles = (juego) => {
+  document.querySelector(".juego__imagen").src = juego.background_image;
+  document.querySelector(".juego__titulo").textContent = juego.name;
+  const lanzamiento = juego.released ? juego.released : "-";
+  document.querySelector(
+    ".juego__fecha"
+  ).textContent = `Lanzamiento: ${lanzamiento}`;
+  const plataformas = Array.isArray(juego.platforms)
+    ? juego.platforms
+        .filter((p) => p.platform && p.platform.name)
+        .map((p) => p.platform.name)
+        .join(", ")
+    : "Plataformas no disponibles";
+
+  document.querySelector(
+    ".juego__plataformas"
+  ).textContent = `Plataformas: ${plataformas}`;
+
+  document.querySelector(".juego__descripcion--text").textContent =
+    juego.description_raw;
+};
+
+// Ejecutar cuando cargue la página
+document.addEventListener("DOMContentLoaded", () => {
+  const id = obtenerIdDeURL();
+  if (id) {
+    pedirDetalles(id);
+  } else {
+    console.error("No se encontró el ID en la URL");
+  }
+});
